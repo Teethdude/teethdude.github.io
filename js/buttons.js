7332c6d06f840e59c84bc3d81bc9a8e668906cd3
Teethy <106205878+Teethdude@users.noreply.github.com>
2024-02-03 10:09:56 -0500

    Add files via upload

---
+++ js/buttons.js
@@ -0,0 +1,127 @@
+//Button Action JavaScript
+
+//USER PAGE OPEN
+function openFormaboutUser() {
+  document.getElementById("aboutUser").style.display = "block";
+}
+
+function closeFormaboutUser() {
+  document.getElementById("aboutUser").style.display = "none";
+}
+//END OF USER PAGE OPEN
+
+//PROJECTS OPEN/CLOSE
+function openFormaboutProj() {
+  document.getElementById("aboutProj").style.display = "block";
+ 
+}
+
+function closeFormaboutProj() {
+  document.getElementById("aboutProj").style.display = "none";
+}
+//PROJECTS README
+function openFormaboutProjRead() {
+  document.getElementById("aboutProjRead").style.display = "block";
+ 
+}
+
+function closeFormaboutProjRead() {
+  document.getElementById("aboutProjRead").style.display = "none";
+}
+//END OF PROJECTS README
+//END OF PROJECTS OPEN/CLOSE
+
+//OPEN DOT MOE
+function openFormMoe() {
+  document.getElementById("aboutMoe").style.display = "block";
+ 
+}
+
+function closeFormMoe() {
+  document.getElementById("aboutMoe").style.display = "none";
+}
+
+//MOE README
+function openFormMoeRead() {
+  document.getElementById("aboutMoeRead").style.display = "block";
+ 
+}
+
+function closeFormMoeRead() {
+  document.getElementById("aboutMoeRead").style.display = "none";
+}
+//END OF MOE README
+//END OF OPEN DOT MOE
+
+//OPEN DOT CA 
+function openFormCA() {
+  document.getElementById("aboutCA").style.display = "block";
+ 
+}
+
+function closeFormCA() {
+  document.getElementById("aboutCA").style.display = "none";
+}
+
+//DOT CA README
+function openFormCARead() {
+  document.getElementById("aboutCARead").style.display = "block";
+ 
+}
+
+function closeFormCARead() {
+  document.getElementById("aboutCARead").style.display = "none";
+}
+//END OF DOT CA README
+//END OF OPEN DOT CA
+
+//ABOUT SYSTEM 
+function openFormaboutSystem() {
+  document.getElementById("aboutSystem").style.display = "block";
+}
+
+function closeFormaboutSystem() {
+  document.getElementById("aboutSystem").style.display = "none";
+}
+//END OF ABOUT SYSTEM
+
+//OPEN MUSIC
+function openFormMusic() {
+  document.getElementById("Music").style.display = "block";
+ 
+}
+
+function closeFormMusic() {
+  document.getElementById("Music").style.display = "none";
+}
+//END OF OPEN MUSIC
+
+//ERROR POPUP
+ var x = document.getElementById("myAudio");
+function openFormaboutError() {
+  document.getElementById("aboutError").style.display = "block";
+  x.play(); 
+}
+
+function closeFormaboutError() {
+  document.getElementById("aboutError").style.display = "none";
+}
+//END OF ERROR POPUP
+
+//PROGRAM MANAGER WINDOW
+function openFormprogramman() {
+  document.getElementById("program-man").style.display = "inline";
+  document.getElementById("PRMico").style.display = "none";
+}
+
+function closeFormprogramman() {
+  document.getElementById("program-man").style.display = "none";
+  document.getElementById("PRMico").style.display = "block";
+}
+//END OF PROGRAM MANAGER WINDOW
+
+//CRASH SYSTEM SIM
+function crashOS() {
+  document.getElementById("crash").style.display = "block";
+}
+//END OF CRASH SYSTEM SIM
